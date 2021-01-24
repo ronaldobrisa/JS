ac798eff944c8cb9a125e4e872ac8471ac058510
ronaldobrisa <familiaberohi@gmail.com>
2021-01-24 19:30:37 -0300

    Novos ajsutes no código

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const dino = document.querySelector('.dino');
 //console.log(dino);
-document.addEventListener('keyup', function()){
-console.log('pressionou tecla')
-});
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        jump();
+    }
+}
+
+function jump() {
+    let position = 0;
+    let upInterval = setInterval(() => {
+        position += 20;
+        dino.style.bottom = position + 'px';
+    }, 20);
+
+}
+
+
+
+document.addEventListener['keyup', handleKeyUp];
+    
+//console.log('pressionou tecla')
+
